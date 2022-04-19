f06b05a00506103e5db6d8830de8818a450b8b75
Anna Ipatova <erosha1988@mail.ru>
2022-04-19 10:29:54 +0300

    initial commit

---
+++ src/main/java/by/itsupportme/gradletask/GradleTaskApplication.java
@@ -0,0 +1,13 @@
+package by.itsupportme.gradletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTaskApplication.class, args);
+    }
+
+}
