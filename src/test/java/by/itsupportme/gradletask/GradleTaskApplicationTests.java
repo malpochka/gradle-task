6bc19be5dea930f26ed0c8a5bf4e42ca66567659
Anna Ipatova <erosha1988@mail.ru>
2022-04-19 16:36:45 +0300

    task implementation

--- src/test/java/by/itsupportme/gradletask/GradleTaskApplicationTests.java
+++ src/test/java/by/itsupportme/gradletask/GradleTaskApplicationTests.java
@@ -3,11 +3,19 @@ package by.itsupportme.gradletask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class GradleTaskApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
+    }
+
+    @Test
+    void contextLoadsSecond() {
+        assertTrue(true);
     }
 
 }
